045ca5df3858841b687df632ce0237ec97dc6e4b
evanhutagaol <evanhutagaol15@gmail.com>
2023-06-27 21:22:28 +0700

    setup homepage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from "./components/Header";
-import Home from './components/Home';
+import React from "react";
+import "./App.css";
+import Home from "./components/Home";
+import { styled } from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
-      <Home />
+      <Container>
+        <Home />
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  height: 100vh;
+`;
