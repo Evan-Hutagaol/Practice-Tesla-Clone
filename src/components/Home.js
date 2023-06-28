c27ceb78d465ad176c379b8b9bb7cb68136eb220
evanhutagaol <evanhutagaol15@gmail.com>
2023-06-28 21:53:50 +0700

    add new content

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,63 @@ import Section from "./Section";
 function Home() {
   return (
     <Container>
-      <Section />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="/model-3.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+        footerDescription="3 months free Supercharging when you take delivery of a new Inventory vehicle by June 30"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Demo Drive"
+      />
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Demo Drive"
+      />
+      <Section
+        title="Solar Panels"
+        description="Schedule a Virtual Consultation"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      />
+
+      <Section
+        title="Solar Roof"
+        description="Produce Clean Energy From Your Roof"
+        backgroundImg="solar-roof.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      />
+
+      <Section
+        title="Powerwall"
+        backgroundImg="powerwall.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      />
+
+      <Section
+        title="Accessories"
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop Now"
+      />
     </Container>
   );
 }
