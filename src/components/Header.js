9c79ef9506fc8046fbbd16794852f6859caf9a64
evanhutagaol <evanhutagaol15@gmail.com>
2023-06-29 09:41:28 +0700

    add sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false);
+
   return (
     <Container>
       <a>
@@ -14,18 +17,73 @@ function Header() {
         <a href="#">Model 3</a>
         <a href="#">Model X</a>
         <a href="#">Model Y</a>
-        {/* <a href="#">Solar Roof</a>
+        <a href="#">Solar Roof</a>
         <a href="#">Solar Panels</a>
-        <a href="#">Powerwall</a> */}
+        <a href="#">Powerwall</a>
 
         <RightMenu>
           <a href="#">Shop</a>
           <a href="#">Account</a>
-          {/* <a href="#">Menu</a> */}
-          <CustomMenu />
+          <CustomMenu onClick={() => setBurgerStatus(true)} />
         </RightMenu>
-
+        <BurgerNav show={burgerStatus}>
+          <CloseWrapper>
+            <CustomClose onClick={() => setBurgerStatus(false)} />
+          </CloseWrapper>
+          <li>
+            <a href="#">Existing Inventory</a>
+          </li>
+          <li>
+            <a href="#">Used Inventory</a>
+          </li>
+          <li>
+            <a href="#">Trade-In</a>
+          </li>
+          <li>
+            <a href="#">Demo Drive</a>
+          </li>
+          <li>
+            <a href="#">Insurence</a>
+          </li>
+          <li>
+            <a href="#">Fleet</a>
+          </li>
+          <li>
+            <a href="#">Cybertruck</a>
+          </li>
+          <li>
+            <a href="#">Roadster</a>
+          </li>
+          <li>
+            <a href="#">Semi</a>
+          </li>
+          <li>
+            <a href="#">Charging</a>
+          </li>
+          <li>
+            <a href="#">Commercial Energy</a>
+          </li>
+          <li>
+            <a href="#">Utilities</a>
+          </li>
+          <li>
+            <a href="#">Careers</a>
+          </li>
+          <li>
+            <a href="#">Find Us</a>
+          </li>
+          <li>
+            <a href="#">Events</a>
+          </li>
+          <li>
+            <a href="#">Supports</a>
+          </li>
+          <li>
+            <a href="#">Investor Relations</a>
+          </li>
+        </BurgerNav>
       </Menu>
+      {burgerStatus && <Backdrop onClick={() => setBurgerStatus(false)} />}
     </Container>
   );
 }
@@ -42,14 +100,14 @@ const Container = styled.div`
   top: 0;
   left: 0;
   right: 0;
+  z-index: 1;
 `;
 
 const Menu = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
-  flex: 1;
-
+  justify-content: flex;
+  flex: 0.85;
   a {
     font-weight: 600;
     flex-wrap: nowrap;
@@ -59,7 +117,7 @@ const Menu = styled.div`
     margin-right: 20px;
   }
 
-  @media(max-width : 768px){
+  @media (max-width: 768px) {
     display: none;
   }
 `;
@@ -67,14 +125,58 @@ const Menu = styled.div`
 const RightMenu = styled.div`
   display: flex;
   align-items: center;
+  margin-left: auto;
+
   a {
     font-weight: 600;
-    margin-right: 10px;
   }
 `;
 
 const CustomMenu = styled(MenuIcon)`
   cursor: pointer;
+`;
 
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background-color: white;
+  width: 300px;
+  list-style: none;
+  z-index: 16;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transition: transform 0.3s;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
 
-`
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+
+    a {
+      font-weight: 600;
+    }
+  }
+`;
+
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`;
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 15;
+`;
