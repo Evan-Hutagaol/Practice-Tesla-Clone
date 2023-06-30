369a61299e79ee3454cfab6a9811e966d5612b08
evanhutagaol <evanhutagaol15@gmail.com>
2023-06-30 09:49:57 +0700

    add description product

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,6 +2,9 @@ import React from "react";
 import { styled } from "styled-components";
 import Fade from "react-reveal/Fade";
 import Zoom from "react-reveal/Zoom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Section({
   title,
@@ -9,26 +12,53 @@ function Section({
   leftBtnText,
   rightBtnText,
   backgroundImg,
+  range1,
+  range2,
+  mph1,
+  mph2,
+  mile1,
+  mile2,
+  peakpower1,
+  peakpower2,
 }) {
   return (
     <Wrap bgImage={backgroundImg}>
       <Zoom>
         <ItemText>
           <h1>{title}</h1>
-          <p>{description}</p>
+          <h5>{description}</h5>
         </ItemText>
       </Zoom>
 
+      <CenteredContainer>
+        <Row>
+          <Col xs={3}>
+            <h3>{range1}</h3>
+            <p>{range2}</p>
+          </Col>
+          <Col xs={3}>
+            <h3>{mph1}</h3>
+            <p>{mph2}</p>
+          </Col>
+          <Col xs={3}>
+            <h3>{mile1}</h3>
+            <p>{mile2}</p>
+          </Col>
+          <Col xs={3}>
+            <h3>{peakpower1}</h3>
+            <p>{peakpower2}</p>
+          </Col>
+        </Row>
+      </CenteredContainer>
       <Button>
-        <ButtonGroup>
-          <Fade left>
+        <Fade bottom>
+          <ButtonGroup>
             <LeftButton>{leftBtnText}</LeftButton>
-          </Fade>
-          <Fade right>
             {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
-          </Fade>
-        </ButtonGroup>
-        <DownArrow src="../assets/images/down-arrow.svg" />
+            {/* <RightButton>{rightBtnText}</RightButton> */}
+          </ButtonGroup>
+        </Fade>
+        {/* <DownArrow src="../assets/images/down-arrow.svg" /> */}
       </Button>
     </Wrap>
   );
@@ -47,6 +77,29 @@ const Wrap = styled.div`
   justify-content: space-between; //vertical column
   align-items: center; //horizontal
   background-image: ${(props) => `url("../assets/images/${props.bgImage}")`};
+  position: relative;
+  overflow: hidden;
+
+  /* Container {
+    padding-top: 20px;
+  } */
+
+  h3 {
+    padding-top: 25%;
+    color: white;
+    font-size: 24px;
+  }
+
+  p {
+    color: white;
+    font-size: 12px;
+    height: 10%;
+    font-weight: bold;
+  }
+
+  h5 {
+    margin-bottom: 100%;
+  }
 `;
 
 const ItemText = styled.div`
@@ -83,10 +136,17 @@ const RightButton = styled(LeftButton)`
   color: black;
 `;
 
-const DownArrow = styled.img`
-  height: 40px;
-  overflow-x: hidden;
-  animation: animateDown infinite 1.5s;
-`;
+// const DownArrow = styled.img`
+//   height: 40px;
+//   overflow-x: hidden;
+//   animation: animateDown infinite 1.5s;
+// `;
 
 const Button = styled.div``;
+
+const CenteredContainer = styled(Container)`
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 25%;
+`;
