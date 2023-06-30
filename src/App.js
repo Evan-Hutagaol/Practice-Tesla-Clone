a12d0ed39830806f5a34742c558444679377b976
evanhutagaol <evanhutagaol15@gmail.com>
2023-06-30 09:58:30 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
-import Header from "./components/Header";
 import { styled } from "styled-components";
+import ModelS from "./pages/ModelS";
+import Model3 from "./pages/Model3";
+import ModelX from "./pages/ModelX";
+import ModelY from "./pages/ModelY";
+import Powerwall from "./pages/Powerwall";
+import SolarPanels from "./pages/SolarPanels";
+import SolarRoof from "./pages/SolarRoof";
+import Header from "./components/Header";
+// import DarkMode from "./components/DarkMode";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+      {/* <DarkMode/> */}
+        <Header />
+        
+
+        <Route exact path="/" component={Home} />
+        <Route exact path="/model-s" component={ModelS} />
+        <Route exact path="/model-3" component={Model3} />
+        <Route exact path="/model-x" component={ModelX} />
+        <Route exact path="/model-y" component={ModelY} />
+        <Route exact path="/powerwall" component={Powerwall} />
+        <Route exact path="/solar-panels" component={SolarPanels} />
+        <Route exact path="/solar-roof" component={SolarRoof} />
+        {/* Define more routes here */}
+        
+      </div>
+      
+    </BrowserRouter>
   );
 }
 
