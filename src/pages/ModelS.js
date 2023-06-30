5c298a8cf40866365d593c720e4ad06aae2b8ec5
evanhutagaol <evanhutagaol15@gmail.com>
2023-07-01 00:21:33 +0700

    implemented Sections

--- src/pages/ModelS.js
+++ src/pages/ModelS.js
@@ -2,6 +2,7 @@ import React from "react";
 import Section from "../components/Section";
 import "bootstrap/dist/css/bootstrap.css";
 import styled from "styled-components";
+import Sections from "../components/Sections";
 
 function ModelS() {
   return (
@@ -20,15 +21,18 @@ function ModelS() {
         mile2="1/4 Mile"
         peakpower1="1,020 hp"
         peakpower2="Peak Power"
-
-        // describe3="200 mph Top Speed*"
-        // describe4="1,020 hp Peak Power"
       />
 
-      <Section
+      <Sections
         title="Interior of the Future"
         backgroundImg="/new-interior.jpg"
-        leftBtnText="Live Demo"
+      />
+
+      <Sections
+        backgroundImg2='/model-s.jpg'
+
+
+
       />
     </Container>
   );
