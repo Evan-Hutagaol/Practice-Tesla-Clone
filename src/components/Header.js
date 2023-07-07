cd60a195b78b5ce664e0ad8dbaf56c1476d06c52
evanhutagaol <evanhutagaol15@gmail.com>
2023-07-07 22:55:58 +0700

    add new css

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import { Link } from "react-router-dom";
 import Fade from "react-reveal/Fade";
-import './DarkMode.css'
+import "./DarkMode.css";
 
 export {
   Header,
@@ -23,25 +23,47 @@ function Header() {
 
   return (
     <Container>
-      <Link to="">
-        <img src="/assets/images/logo.svg" alt="Tesla Logo" />
-      </Link>
+      <Menu className="menuBar">
+        <Link to="" className="logo">
+          <img
+            src="/assets/images/logo.svg"
+            alt="Tesla Logo"
+            style={{ backgroundColor: "white" }}
+          />
+        </Link>
+        <li>
+          <Link to="/model-s">Model S</Link>
+        </li>
+        <li>
+          <Link to="/model-3">Model 3</Link>
+        </li>
+        <li>
+          <Link to="/model-x">Model X</Link>
+        </li>
+        <li>
+          <Link to="/model-y">Model Y</Link>
+        </li>
+        <li>
+          <Link to="/solar-roof">Solar Roof</Link>
+        </li>
+        <li>
+          <Link to="/solar-panels">Solar Panels</Link>
+        </li>
+        <li>
+          <Link to="/powerwall">Powerwall</Link>
+        </li>
 
-      <Menu>
-        <Link to="/model-s">Model S</Link>
-        <Link to="/model-3">Model 3</Link>
-        <Link to="/model-x">Model X</Link>
-        <Link to="/model-y">Model Y</Link>
-        <Link to="/solar-roof">Solar Roof</Link>
-        <Link to="/solar-panels">Solar Panels</Link>
-        <Link to="/powerwall">Powerwall</Link>
-
-        <RightMenu>
-          <Link to="/shop">Shop</Link>
-          <Link to="/account">Account</Link>
-
+        <RightMenu className="">
+          <li>
+            <Link to="/shop">Shop</Link>
+          </li>
+          <li>
+            <Link to="/account">Account</Link>
+          </li>
           <CustomMenu onClick={() => setBurgerStatus(true)} />
         </RightMenu>
+      </Menu>
+      <Menu className="sidebarMenu">
         <BurgerNav show={burgerStatus}>
           <CloseWrapper>
             <CustomClose onClick={() => setBurgerStatus(false)} />
@@ -106,8 +128,11 @@ function Header() {
 
 export default Header;
 
+// const Container = styled.div`
+// `;
+
 const Container = styled.div`
-  min-height: 60px;
+  /* min-height: 60px;
   position: fixed;
   display: flex;
   align-items: center;
@@ -116,44 +141,66 @@ const Container = styled.div`
   top: 0;
   left: 0;
   right: 0;
-  z-index: 1;
+  z-index: 1; */
 `;
 
 const Menu = styled.div`
   display: flex;
   align-items: center;
-  justify-content: flex;
+  justify-content: center;
   flex: 0.85;
 
 
-  a {
-    font-weight: 600;
-    flex-wrap: nowrap;
-    padding: 0 10px;
-    text-decoration: none;
-    color: black;
-    position: relative;
-    margin: 20px;
+  &.menuBar {
+    background-color: white;
+    color: white;
+    min-height: 60px;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1;
+    list-style: none;
+
+    li {
+      a {
+        font-weight: 600;
+        flex-wrap: nowrap;
+        padding: 0 10px;
+        text-decoration: none;
+        color: black;
+        position: relative;
+        margin: 20px;
+        align-items: center;
+      }
+
+      a:after {
+        content: "";
+        position: absolute;
+        background-color: black;
+        height: 3px;
+        width: 0;
+        left: 0;
+        bottom: -7px;
+        transition: 0.5s;
+        /* transform: translateX(-0%); */
+      }
+
+      a:hover:after {
+        width: 100%;
+      }
+    }
   }
 
-  /* a:hover{
-    color: whitesmoke;
-  } */
-
-  a:after{
-    content: '';
-    position: absolute;
-    background-color: black;
-    height: 3px;
-    width: 0;
-    left:0;
-    bottom: -7px;
-    transition: 0.5s;
-    /* transform: translateX(-0%); */
-  }
-
-  a:hover:after{
-    width: 100%;
+  &.sideBarMenu {
+    a {
+      text-decoration: none;
+      color: black;
+    }
   }
 
   @media (max-width: 768px) {
@@ -170,6 +217,8 @@ const RightMenu = styled.div`
     font-weight: 600;
   }
 `;
+
+
 
 const CustomMenu = styled(MenuIcon)`
   cursor: pointer;
@@ -195,12 +244,15 @@ const BurgerNav = styled.div`
     padding: 15px 0;
     a {
       font-weight: 600;
+      text-decoration: none;
+      color: black;
     }
   }
 `;
 
 const CustomClose = styled(CloseIcon)`
   cursor: pointer;
+  background-color: white;
 `;
 
 const CloseWrapper = styled.div`
